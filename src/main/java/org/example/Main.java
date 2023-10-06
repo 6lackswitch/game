42c09ec0b07797b0ecf25d2a3166db60909cd90c
6lackswitch <6lackswitch@gmail.com>
2023-10-06 19:09:43 +0300

    feat: project created

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Добро пожаловать в игру");
+        Scanner scanner = new Scanner(System.in);
+        Battle battle = new Battle(scanner);
+        battle.round();
+        System.out.println("Игра окончена. Чтобы повторить введите \"retry\". Для выхода любое другое значение");
+        String input = scanner.nextLine();
+        if (input.equals("retry")) {
+            main(args);
+        }
+    }
+}
---
+++ src/main/java/org/example/models/Entity.java
@@ -0,0 +1,53 @@
+package org.example.models;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public abstract class Entity {
+    protected String name;
+    protected int health;
+    protected int attack;
+    protected int[] damage;
+    protected int armor;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int[] getDamage() {
+        return damage;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public void attack(Entity entity) {
+        int difference = this.getAttack() - entity.getArmor() + 1;
+        int[] cubeValues = new int[difference];
+        for (int i = 0; i < difference; i++) {
+            cubeValues[i] = new Random().nextInt(6 - 1) + 1;
+        }
+        System.out.println("Кубик брошен. Значения " + Arrays.toString(cubeValues));
+        if(Arrays.stream(cubeValues).anyMatch(x -> x >= 5)) {
+            int rnd = new Random().nextInt(this.getDamage().length);
+            entity.takeDamage(this.getDamage()[rnd]);
+            System.out.println("Сущность " + this.getName() + " нанес сущности " + entity.getName() + " " + this.getDamage()[rnd] + " урона");
+            System.out.println("Здоровье " + entity.getName() + " равно " + entity.getHealth());
+        } else {
+            System.out.println("Не повезло");
+        }
+    }
+
+    public void takeDamage(int damage) {
+        this.health = this.health - damage;
+    }
+}
